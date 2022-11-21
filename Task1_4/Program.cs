abfb42bb3a65fe041e94d52e85ae49fdc84d009b
alexrozanov <alexrozans@gmail.com>
2022-11-22 00:37:02 +0300

    task1_4 in progress

--- Task1_4/Program.cs
+++ Task1_4/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int с = Convert.ToInt32(Console.ReadLine());
+int[] numbers = { a, b, c }
