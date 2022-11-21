95a52878503ab9bd0fdca2224b8f6ca7512a0785
alexrozanov <alexrozans@gmail.com>
2022-11-21 23:57:06 +0300

    task 1_2 is done

---
+++ Task1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+while ( a == b )
+{
+    Console.Write("Числа равны \nВведите другие числа: "); 
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+}
+if(a > b)
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(a);
+    Console.Write("Меньшее число ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(b);
+    Console.Write("Меньшее число ");
+    Console.WriteLine(a);
+}
