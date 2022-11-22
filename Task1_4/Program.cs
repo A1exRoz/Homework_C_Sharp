a3297c4c6b7d49d06892211adcdc57922b2989a3
alexrozanov <alexrozans@gmail.com>
2022-11-22 21:56:23 +0300

    task 1_4 in progress

--- Task1_4/Program.cs
+++ Task1_4/Program.cs
@@ -1,8 +1,25 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
 Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
-int с = Convert.ToInt32(Console.ReadLine());
-int[] numbers = { a, b, c }
+int c = Convert.ToInt32(Console.ReadLine());
+int[] num = new Int32[] { a, b, c};
+int i = 0;
+int max = num[i];
+int countMax = 0;
+while (i < num.Length)
+{
+    if (num[i] > max)
+    {
+        max = num[i];
+        countMax = 1;
+    }
+    else 
+    {
+        if (num[i] == max)
+            countMax
+}
