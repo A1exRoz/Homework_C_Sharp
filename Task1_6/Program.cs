c883423942c0fc5fa7f51ae00c3cd5b885e182f6
alexrozanov <alexrozans@gmail.com>
2022-11-22 17:48:17 +0300

    task 1_6 done

---
+++ Task1_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ( n % 2 == 0)
+    Console.WriteLine("Вы ввели четное число");
+else
+    Console.WriteLine("Вы ввели нечетное число");
