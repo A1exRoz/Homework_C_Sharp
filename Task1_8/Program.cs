41e827d84a591171be43de960ab1f6d7f18aea4e
alexrozanov <alexrozans@gmail.com>
2022-11-22 18:54:14 +0300

    task 1_8 done

---
+++ Task1_8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напиcать программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;
+// while (i <= n && i % 2 == 0)
+// {
+//     Console.Write($"{i} ");
+//     i++;
+// }
+
+for (int i = 1; i <= n; i++) 
+{ 
+    if (i % 2 == 0) 
+    Console.Write (i + "  " ); 
+}
