9daa386327555103dbbb8d836469442a6beb5d0f
alexrozanov <alexrozans@gmail.com>
2022-11-28 10:13:19 +0300

    edit before merge

--- Task2_add/Program.cs
+++ Task2_add/Program.cs
@@ -11,13 +11,14 @@ while (n != 0)
 {
     if (n > max1)
     {
-        max2 == max1;
-        max1 == n;
+        max2 = max1;
+        max1 = n;
     }
-    else (n > max2);
+    else if (n > max2);
     {
-        max2 == n;
+        max2 = n;
     }
     n = Convert.ToInt32(Console.ReadLine());
 }
-Console.Write("end");
+Console.Write("Второе максимальное число: ");
+Console.WriteLine(max2);
