7840579cace7c13d4b49d8ee237b377ea5b0ff27
alexrozanov <alexrozans@gmail.com>
2022-11-29 10:01:35 +0300

    task 3 19 is done

--- Task3_19/Program.cs
+++ Task3_19/Program.cs
@@ -8,16 +8,16 @@ Console.Clear();
 
 Console.Write("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-while (n > 99999 && n < 10000);
+while (n <= 9999 || n > 99999)
 {
     Console.Write("Вы ввели не верное число, введите 5-ти значное число: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
-if ((n % 10 == n / 10000) && ((n / 100) % 10 == (n / 10000) % 10))
+if ((n % 10 == n / 10000) && ((n / 10) % 10 == (n / 1000) % 10))
 {
     Console.WriteLine("Число является палиндромом");
 }
 else
 {
-    Console.WriteLine("Число является палиндромом");
+    Console.WriteLine("Число НЕ является палиндромом");
 }
