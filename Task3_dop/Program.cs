c05b12c52da0580c2a207cbdce72091fb3372546
alexrozanov <alexrozans@gmail.com>
2022-11-30 19:19:57 +0300

    edit task 3 dop

--- Task3_dop/Program.cs
+++ Task3_dop/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.Clear();
 
+//Условие
 Console.Write("Введите колличество кустов на поляне, от 3 до 1000: ");
 int n = Convert.ToInt32(Console.ReadLine());
 while (n < 3 || n > 1000)
@@ -10,12 +11,25 @@ while (n < 3 || n > 1000)
 int[] a = new int[n];
 Console.WriteLine("Введите колличество ягод на каждом кусте: ");
 for (int i = 0; i < n; i++)
+{
     a[i] = Convert.ToInt32(Console.ReadLine());
-
-int max = 0;
-int i = 0;
-if (i <= n - 1) 
-{ 
-    (a[i] + a[i + 1] + a[i + 2]) > max;
-    sum > max;
 }
+
+//Решение
+int maxsum = 0;
+int cursum = 0;
+for (int i = 0; i < n - 2; i++)
+{
+    cursum = a[i] + a[i + 1] + a[i + 2];
+    if (cursum > maxsum) 
+    { 
+        maxsum = cursum;
+    }
+}
+if (a[0] + a[n - 1] + a[n - 2] > maxsum)
+    maxsum = a[0] + a[n - 1] + a[n - 2];
+
+else if (a[0] + a[1] + a[-1] > maxsum)
+    maxsum = a[0] + a[1] + a[n - 1];
+
+Console.Write(maxsum);
