cc6465e7335e84e22dd2bfddcac81dd3dd859ad2
alexrozanov <alexrozans@gmail.com>
2022-12-01 12:00:05 +0300

    new decision tasks after seminar

--- Task3_19/Program.cs
+++ Task3_19/Program.cs
@@ -10,7 +10,7 @@ Console.Write("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 while (n <= 9999 || n > 99999)
 {
-    Console.Write("Вы ввели не верное число, введите 5-ти значное число: ");
+    Console.Write("Вы ввели не верное число! \nвведите 5-ти значное число: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
 if ((n % 10 == n / 10000) && ((n / 10) % 10 == (n / 1000) % 10))
@@ -21,3 +21,48 @@ else
 {
     Console.WriteLine("Число НЕ является палиндромом");
 }
+
+
+// Вот так еще решали эту задачу:
+
+// Console.Write("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// while (n < 10000 || n > 99999)
+// {
+//     Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+//     n = Convert.ToInt32(Console.ReadLine());
+// }
+// int n1 = n / 10000;
+// int n2 = (n / 1000) % 10;
+// int n4 = (n % 100) / 10;
+// int n5 = n % 10;
+
+// if (n1 == n5 && n2 == n4)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+
+// Через подсчет строк:
+
+// Console.Write("Введите пятизначное число: ");
+// string? n = Console.ReadLine();
+// if (n[0] == n[4] && n[1] == n[3])
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+
+// Вообще любое число можно проверить вот так:
+
+// Console.Write("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = n;
+// int newNumber = 0, i = Convert.ToString(n).Length - 1;
+// while (i >= 0)
+// {
+//     newNumber = newNumber + (n % 10 * Convert.ToInt32(Math.Pow(10, i)));
+//     n = n / 10;
+//     i--;
+// }
+// Console.WriteLine(newNumber == result);
--- Task3_dop/Program.cs
+++ Task3_dop/Program.cs
@@ -33,3 +33,29 @@ if (a[0] + a[1] + a[-1] > maxsum)
     maxsum = a[0] + a[1] + a[n - 1];
 
 Console.Write(maxsum);
+
+
+
+// Решение Дениса на семинаре:
+
+// Console.Clear();
+// Console.Write("Введите кол-во кустов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] arrayFirst = new int[n];
+// int resultSumma = 0;
+
+// for (int i = 0; i < arrayFirst.Length; i++)
+//     arrayFirst[i] = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i < arrayFirst.Length - 1; i++)
+// {
+//     if (arrayFirst[i - 1] + arrayFirst[i] + arrayFirst[i + 1] > resultSumma)
+//         resultSumma = arrayFirst[i - 1] + arrayFirst[i] + arrayFirst[i + 1];
+// }
+// if (arrayFirst[0] + arrayFirst[1] + arrayFirst[n - 1] > resultSumma)
+//     resultSumma = arrayFirst[0] + arrayFirst[1] + arrayFirst[n - 1];
+    
+// if (arrayFirst[0] + arrayFirst[n - 1] + arrayFirst[n - 2] > resultSumma)
+//     resultSumma = arrayFirst[0] + arrayFirst[n - 1] + arrayFirst[n - 2];
+
+// Console.WriteLine(resultSumma);
