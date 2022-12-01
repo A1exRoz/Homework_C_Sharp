363a4d77a2e534467869baf950cf958d2634bd3f
alexrozanov <alexrozans@gmail.com>
2022-12-01 23:19:44 +0300

    task4 27 is done

---
+++ Task4_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая
+// принимает на вход число и выдаёт сумму цифр в
+// числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (n / 10 != 0)
+{
+    result += n % 10;
+    n /= 10;
+}
+result += n;
+Console.WriteLine(result);
