c615e3f0f26c1ff7c35d14f4df33149e317baf67
alexrozanov <alexrozans@gmail.com>
2022-12-01 23:38:32 +0300

    task4 dop in progress

---
+++ Task4_dop/Program.cs
@@ -0,0 +1,32 @@
+﻿// Дана последовательность из N целых чисел и число K. 
+// Необходимо сдвинуть всю последовательность (сдвиг - циклический)
+// на |K| элементов вправо, если K – положительное 
+// и влево, если отрицательное.
+
+// Первая строка содержит натуральное число N, 
+// во второй строке записаны N целых чисел Ai, 
+// а в последней – целое число K. (1 ≤ N ≤ 105, |K| ≤ 105, |Ai| ≤ 100).
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(0, 10);
+
+int count = array.Length;
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }   
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+Console.Write(array);
