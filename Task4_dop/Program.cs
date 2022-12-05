5156f3aeb5d7aaa73d658477802f83dce92566b8
alexrozanov <alexrozans@gmail.com>
2022-12-05 11:52:50 +0300

    task 4 dop is done

--- Task4_dop/Program.cs
+++ Task4_dop/Program.cs
@@ -9,24 +9,31 @@
 
 
 Console.Clear();
-Console.Write("Введите кол-во элементов в массиве: ");
+Console.Write("Введите кол-во элементов массивва: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
-for (int i = 0; i < n; i++)
-    array[i] = new Random().Next(0, 10);
+int[] arrayHelp = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
 
-int count = array.Length;
-
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
-
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if(array[j] < array[minPosition]) minPosition = j;
-        }   
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-Console.Write(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int k = Convert.ToInt32(Console.ReadLine());
+k = k % n;
+if (k > 0)
+{
+    for (int i = 0; i < k; i++)
+        arrayHelp[i] = array[array.Length - k + i];
+    for (int i = 0; i < array.Length - k; i++)
+        arrayHelp[k + i] = array[i];
+    
+    Console.WriteLine($"[{string.Join(", ", arrayHelp)}]");
+}
+else
+{
+    k = (-1) * k;
+    for (int i = 0; i < k; i++)
+        arrayHelp[array.Length - k + i] = array[i];
+    for (int i = 0; i < array.Length - k; i++)
+        arrayHelp[i] = array[k + i];
+    Console.WriteLine($"[{string.Join(", ", arrayHelp)}]");
+}
