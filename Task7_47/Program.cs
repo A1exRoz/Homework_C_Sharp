c1674ad7fcd243e115a69903adfba8156b1c5f0f
alexrozanov <alexrozans@gmail.com>
2022-12-14 17:30:23 +0300

    task 7 47 is done

--- Task7_47/Program.cs
+++ Task7_47/Program.cs
@@ -7,3 +7,22 @@
 // 1       -3,3       8        -9,9
 // 8        7,8      -7,1       9
 
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (20 - 10) + 10, 2); // new Random().Next(0, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+double[,] matrix = new double[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
