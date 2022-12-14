0e626b2058a51d0e3230f22bc5c3ccce96831e32
alexrozanov <alexrozans@gmail.com>
2022-12-14 19:47:48 +0300

    task 7 50 is done

--- Task7_50/Program.cs
+++ Task7_50/Program.cs
@@ -7,3 +7,30 @@
 // 5 9 2 3
 // 8 4 2 4
 // 1 7 -> такой позиции в массиве нет
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
+Console.WriteLine("Введите искомую позицию: ");
+string[] find = Console.ReadLine().Split(" "); 
+
+if (int.Parse(find[0]) > matrix.GetLength(0) - 1 || int.Parse(find[1]) > matrix.GetLength(1) - 1) 
+    Console.WriteLine("Такой позиции нет"); 
+else 
+    Console.WriteLine(matrix[int.Parse(find[0]), int.Parse(find[1])]);
