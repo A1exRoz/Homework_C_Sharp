839e5e7e574fbcac27e459c8a37e94ffe51421c2
alexrozanov <alexrozans@gmail.com>
2022-12-18 13:59:46 +0300

    task 8 56 is done

--- Task8_56/Program.cs
+++ Task8_56/Program.cs
@@ -5,5 +5,48 @@
 // 5 9 2 3
 // 8 4 2 4
 // 5 2 6 7
-// Программа считает сумму элементов в каждой строке 
-// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void inputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void numberRowMinSum(int[,] matrix)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) sumRow += matrix[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальный массив: ");
+inputMatrix(matrix);
+Console.WriteLine();
+numberRowMinSum(matrix);
